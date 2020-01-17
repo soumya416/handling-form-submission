12544f212b03ad8f6c6bb624cec0c9a7e9464cb7
cen039 <cen039@192.168.0.134>
2020-01-17 18:44:48 +0530

    commit emprepository

---
+++ handling-form-submission/src/main/java/com/example/handling/model/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.example.handling.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+    List<Employee> findByName(String name);
+    
+
+}
